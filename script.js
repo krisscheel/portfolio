59051640aeab2c828e8cc20b8633ecd731b80abe
Kristen Scheel <kristen.scheel@gmail.com>
2020-08-26 14:24:46 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// ==================== PORTFOLIO ========================
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+
+const myApp = {};
+
+
+//Document ready: 
+$(function () {
+  myApp.init();
+});
